5d4874d54c249b67f44481677bae33cd7f3681f9
Aayush kumar <aayuxhhere@gmail.com>
2026-07-29 21:29:12 +0530

    Time: 0 ms (100%), Space: 95.1 MB (62.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -12,7 +12,7 @@ public:
         int high = m;
         while(low<=high){
             int cut1 = (low+high)/2;
-            int cut2 = (m + n + 1)/2 - cut1;
+            int cut2 = (m + n )/2 - cut1;
             int l1 = (cut1==0) ? INT_MIN : nums1[cut1-1];
             int l2 = (cut2==0) ? INT_MIN : nums2[cut2-1];
 
@@ -24,7 +24,7 @@ public:
                     return ((max(l1,l2) + min(r1,r2))/2.0);
                 }
                 else{
-                    return max(l1,l2);
+                    return min(r1,r2);
                 }
             }
             else if(l1 > r2){
