836573b06c2b3b2710e78e92f8cea1919a093c6f
Aayush kumar <aayuxhhere@gmail.com>
2026-08-02 22:45:57 +0530

    Time: 7 ms (20.62%), Space: 18.9 MB (5.22%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -5,12 +5,15 @@ public:
         for(char ch : s){
             freq[ch]++;
         }
-        sort(s.begin(), s.end(), [&](char a, char b){
-            if(freq[a]==freq[b]){
-                return a>b;
-            }
-            return freq[a] > freq[b];
-        });
-        return s;
+        vector<string> bucket(s.size()+1);
+        for(auto x : freq){
+            bucket[x.second] += x.first;
+        }
+        string ans;
+        for(int i = s.size(); i>=1; i--){
+            for(char c : bucket[i])
+                ans.append(i, c);
+        }
+        return ans;
     }
 };
