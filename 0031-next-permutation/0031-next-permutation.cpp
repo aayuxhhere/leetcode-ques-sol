1b53cb73e74a9bcce6955e8262bba7dbf2d6c79b
Aayush kumar <aayuxhhere@gmail.com>
2026-08-03 23:21:31 +0530

    Time: 0 ms (100%), Space: 15.9 MB (11.86%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        //2 1 5 4 3 0 0 find pivot -- step 1
+        //2 3 5 4 1 0 0 swap with just greater number
+        //2 3 0 0 1 4 5
+
+        //5 4 3 2 1
+        //1 2 3 4 5 
+        int index = -1;
+        for(int i = nums.size()-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for(int i = nums.size()-1; i>=index; i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin()+index+1, nums.end());
         return;
     }
 };
